d045f2a2825bb0e387e5152f055db3ef79f81f49
Nick0611 <13130479633@163.com>
2020-05-17 19:25:24 +0800

    'first'

---
+++ VO/VO.cpp
@@ -0,0 +1,13 @@
+﻿#include<opencv2\core.hpp>
+#include<opencv2\highgui.hpp>
+
+using namespace cv;
+
+int main()
+{
+	Mat img = imread("111.jpg");
+	namedWindow("noticeable");
+	imshow("noticeable", img);
+	waitKey(6000);
+	return 0;
+}
